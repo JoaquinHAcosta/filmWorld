cd60fb8c96742a0439f0ec8b3f2c44a1502bb9ea
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-05 20:26:27 -0300

    searchbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,26 @@
 import Link from 'next/link'
 import React from 'react'
+import { SearchBar } from './SearchBar'
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-between items-center h-20 mx-8 bg-transparent border-b-8 ">
-      <section className="flex">
-        <h1 className="w-[160px] text-center">FilmWorld</h1>
-        <div className="flex gap-4">
-          <Link href="#">Home</Link>
-          <Link href="#">Movies</Link>
-          <Link href="#">Series</Link>
-          <Link href="#">Celebrities</Link>
-        </div>
-      </section>
-      <section className="flex gap-4">
-        <Link href="#">My account</Link>
-        <Link href="#">Login</Link>
-      </section>
-    </nav>
+    <header className="max-w-6xl mx-auto border-b-8">
+      <nav className=" flex justify-between items-center h-20 bg-transparent border-b-8 ">
+        <section className="flex">
+          <h1 className="w-[160px] text-center">FilmWorld</h1>
+          <div className="flex gap-4">
+            <Link href="#">Home</Link>
+            <Link href="#">Movies</Link>
+            <Link href="#">Series</Link>
+            <Link href="#">Celebrities</Link>
+          </div>
+        </section>
+        <section className="flex gap-4 pr-4">
+          <Link href="#">My account</Link>
+          <Link href="#">Login</Link>
+        </section>
+      </nav>
+      <SearchBar />
+    </header>
   )
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const SearchBar = () => {
+  return (
+    <div className="flex">
+      <select name="typeShow">
+        <option value="movie">Movies</option>
+        <option value="serie">TV Series</option>
+      </select>
+      <input type="text" className="grow" />
+    </div>
+  )
+}
