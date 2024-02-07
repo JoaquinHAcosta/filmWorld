8cca7a772336db3823ea041595d87f7d18fc236a
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-07 13:23:45 -0300

    query and apicall working

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,21 +1,24 @@
 'use client'
 
+import { Movie } from '@/types/Movie'
 import { useQuery } from '@tanstack/react-query'
 import axios from 'axios'
 
 const HomePage = () => {
-  const {} = useQuery({
+  const { data, isLoading, isError } = useQuery({
     queryKey: ['getMovies'],
     queryFn: async () => {
       const { data } = await axios.get('/api/movies')
-      console.log(data)
+      return data.results as Movie[]
     },
   })
 
+  if (isLoading) return <div>Loading Movies...</div>
+  if (isError) return <div>Something went wrong, try again</div>
   return (
     <section className="max-w-6xl mx-auto border-b-8">
       <h1>Header movies</h1>
-      <div>aca mapeo las pelis</div>
+      <div>{JSON.stringify(data)}</div>
     </section>
   )
 }
