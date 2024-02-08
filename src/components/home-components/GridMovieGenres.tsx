93090c7f17b56ed92024cb41493870847ad31346
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-07 22:40:59 -0300

    pagination style and logic

--- src/components/home-components/GridMovieGenres.tsx
+++ src/components/home-components/GridMovieGenres.tsx
@@ -3,6 +3,8 @@ import { Movie } from '@/types/Movie'
 import { useQuery } from '@tanstack/react-query'
 import { MovieCard } from '../cards/MovieCard'
 import { useState } from 'react'
+import { LeftArrow } from '../icons/LeftArrow'
+import { RightArrow } from '../icons/RightArrow'
 
 export const GridMovieGenres = ({ pickedGenre }: { pickedGenre: number }) => {
   const [currentPage, setCurrentPage] = useState(1)
@@ -27,7 +29,7 @@ export const GridMovieGenres = ({ pickedGenre }: { pickedGenre: number }) => {
   if (isError)
     return <div className="text-white">Something went wrong, try again</div>
 
-  const N_OF_CARDS = 6
+  const N_OF_CARDS = 8
 
   const totalPages = Math.ceil(data!.length / N_OF_CARDS)
   const start = (currentPage - 1) * N_OF_CARDS
@@ -36,15 +38,35 @@ export const GridMovieGenres = ({ pickedGenre }: { pickedGenre: number }) => {
   let viewMovies = data!.slice(start, end)
   return (
     <div>
-      <div className="grid grid-cols-3">
+      <div className="h-[84vh] grid grid-cols-4">
         {viewMovies.map((movie) => (
           <MovieCard key={movie.id} {...movie} />
         ))}
       </div>
-      <div className="flex justify-center">
-        {currentPage > 1 && <button onClick={prevPage}>PREV</button>}
-        <div>{currentPage}</div>
-        {currentPage < totalPages && <button onClick={nextPage}>NEXT</button>}
+      <div className="flex justify-center border-t-2 border-b-2 py-1">
+        <button
+          className="mr-3 disabled:text-gray-300"
+          disabled={currentPage === 1}
+          onClick={prevPage}
+        >
+          <LeftArrow />
+        </button>
+        <div>
+          {Array.from(Array(totalPages)).map((_, i) => {
+            return (
+              <button className="mx-2" key={i}>
+                {i + 1}
+              </button>
+            )
+          })}
+        </div>
+        <button
+          disabled={currentPage === totalPages}
+          className="ml-3 disabled:text-gray-300"
+          onClick={nextPage}
+        >
+          <RightArrow />
+        </button>
       </div>
     </div>
   )
