f4f64d4ab2c20d3c11f22197165e5945085c3aba
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-12 23:14:13 -0300

    footer;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import { Navbar } from '@/components/Navbar'
 import TanstackProvider from '@/providers/TanstackProvider'
+import { Footer } from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <TanstackProvider>
           <Navbar />
           {children}
+          <Footer />
         </TanstackProvider>
       </body>
     </html>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,10 @@
+import Image from 'next/image'
 import React from 'react'
 
 export const Footer = () => {
-  return <footer className="flex justify-center">Footer</footer>
+  return (
+    <footer className="flex justify-center">
+      <Image src={'/footer-bg.jpg'} alt="footer" width={800} height={240} />
+    </footer>
+  )
 }
