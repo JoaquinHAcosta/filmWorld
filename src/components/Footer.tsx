0a27f10a6f85df5309a8faa8f43b47e0ef80fa3d
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-12 23:47:42 -0300

    this works better;

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,17 +3,12 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="flex justify-center w-full">
-      <Image
-        className="absolute"
-        src="/footer-bg.jpg"
-        alt="footer"
-        width={1800}
-        height={800}
-      />
-      <div className="relative bg-transparent flex justify-between items-center text-white z-10">
-        <div className="bg-red-500">Left</div>
-        <div className="bg-blue-500">Right</div>
+    <footer className="h-96 w-full bg-[url('/footer-bg.jpg')] text-white grid grid-cols-2">
+      <div className="flex justify-center items-center bg-transparent">
+        Left
+      </div>
+      <div className="flex justify-center items-center bg-transparent">
+        Right
       </div>
     </footer>
   )
