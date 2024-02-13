14b67856a639b56bef10c45f005d2529d786e403
Joaquin Acosta <joaquinacosta397@outlook.com>
2024-02-12 23:29:38 -0300

    testing relative

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,18 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <footer className="flex justify-center">
-      <Image src={'/footer-bg.jpg'} alt="footer" width={800} height={240} />
+    <footer className="flex justify-center w-full">
+      <Image
+        className="absolute"
+        src="/footer-bg.jpg"
+        alt="footer"
+        width={1800}
+        height={800}
+      />
+      <div className="relative bg-transparent flex justify-between items-center text-white z-10">
+        <div className="bg-red-500">Left</div>
+        <div className="bg-blue-500">Right</div>
+      </div>
     </footer>
   )
 }
